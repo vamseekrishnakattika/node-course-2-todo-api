be0f7c5a2e633a8553bd1c03e6c4173962c4e1a6
Vamseekrishna Kattika <vamseekrishna.kattika@gmail.com>
2019-02-12 11:59:34 -0600

    Add Post /todos route and refactor mangoose

---
+++ server/server.js
@@ -0,0 +1,89 @@
+var {mangoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  },(err) =>{
+    res.status(400).send(err);
+  })
+});
+
+app.listen(3000,() => {
+  console.log('Started server on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // var newTodo = new Todo({
+// //   text:'Cook dinner'
+// // });
+// //
+// // newTodo.save().then((doc) =>{
+// //   console.log('Saved todo',doc);
+// // },(err) =>{
+// //   console.log('Unable to save todo',err);
+// // });
+//
+// // var otherTodo = new Todo({
+// //   text:'Something to do'
+// // });
+// //
+// // otherTodo.save().then((doc)=>{
+// //   console.log(JSON.stringify(doc,undefined,2));
+// // },(err)=>{
+// //   console.log('Unable to save',err);
+// // });
+//
+//
+// var user = new User({
+//   email:'some@example.com'
+// });
+//
+// user.save().then((doc) =>{
+//   console.log(JSON.stringify(doc,undefined,2));
+// },(err) => {
+//   console.log('Unable to save user',err);
+// });
