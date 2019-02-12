3764bf9e22b860d21fd6af2aa36b1a7afc31ef3b
Vamseekrishna Kattika <vamseekrishna.kattika@gmail.com>
2019-02-12 16:46:38 -0600

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,39 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 })
+
+describe('DELETE /todos/:id',() =>{
+  it('should remove a todo',(done) => {
+    var hexId = todos[0]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      }).end((err,res) =>{
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) =>{
+          expect(todo).toBeFalsy()
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+  it('should return 404 if todo not found',(done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+  it('should return 404 if object id is invalid',(done) => {
+    var hexId = 123;
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+});
